d4951e09dda00048b4267b5ec73b8a5136a26f12
Nugetzrul3 <popingcorn12@gmail.com>
2021-07-25 12:14:02 +1000

    [Core] Update config.env and deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,14 @@ npm run dist
 # navigate into the build output directory
 cd dist
 
+# create CNAME file
+echo $CNAME > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:dogecash/esplora.git master:gh-pages
+git push -f git@github.com:$REPOSITORY master:gh-pages
 
 cd -
